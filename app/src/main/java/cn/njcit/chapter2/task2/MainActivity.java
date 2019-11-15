791cd4280d4456adbaf2597de9b9385f9405b305
LiuXine <Liuxe@njcit.cn>
2019-11-15 09:41:41 +0800

    task2

---
+++ app/src/main/java/cn/njcit/chapter2/task2/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.njcit.chapter2.task2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
